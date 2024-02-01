ec95630b4b82d8082040abbb4bd778c465e8edcd
Megan L Harson <150634753+mharson@users.noreply.github.com>
2024-01-31 19:51:21 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//1. The first number is double five.
+//2. The second number is four times the first number.
+//3. The last number is one less than the second number.
+
+let firstnumber =2*5;
+let secondnumber=4*firstnumber;
+let thirdnumber=secondnumber-1;
+
+//this is the alert that will appear to the user
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${firstnumber}-${secondnumber}-${thirdnumber}`);
